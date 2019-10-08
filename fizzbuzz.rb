ae03dbda1743e5104a7e25ea4f6cf036e7690d2e
yuki <j1634@matsue-ct.jp>
2019-10-08 17:18:29 +0900

    3かつ5の倍数の処理

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,8 @@
 
 (1..100).each{|i|
    if i%3==0 && i%5==0 then
-   if i%3==0 then
+      print("FizzBuzz\n")
+   elsif i%3==0 then
       print("Fizz\n")
    elsif i%5==0 then
       print("Buzz\n")
